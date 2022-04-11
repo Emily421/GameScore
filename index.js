e1e566816f9253ea76d0f1fccc2b351de394fc68
cthompson4 <connort9345@gmail.com>
2022-04-11 01:28:18 -0500

    Added database functionality

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const express = require('express')
 app = express()
+// MongoDB database - tutorial from https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb-how-to-get-connected-to-your-database
+const {MongoClient, Db} = require('mongodb');
+
+async function getRandomWord(client){
+  max = 4; //number of documents plus 1
+  random = Math.floor(Math.random() * max);
+  const randomdoc = await client.db("gamescore_words").collection("words").findOne({number: random});
+  randomword = randomdoc.name;
+}
+
+async function main() {
+	const uri = "mongodb+srv://gamescoreapplication:gamescore@gamescore.hhvnf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+  try {
+    await client.connect();
+    // Call function
+    await  getRandomWord(client);
+
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
+main().catch(console.error);
+//
 
 var url = require('url');
 const path = require('path');
@@ -10,13 +36,13 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'static'))
 
 app.get('/',(request,response)=>{
-    response.render('home.ejs')
+    response.render('home.ejs', {randomword: randomword,})
 })
 
 app.get('/random', (request, response) => {
-	console.log('Calling Random on Node server.')
+	console.log('Calling random word.')
 	response.type('text/plain')
-	response.send('Random word.')
+	response.send(randomword);
 })
 
 app.get('/proposal', (request, response) => {
