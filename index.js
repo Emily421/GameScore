a0a7346126dda38ec0f71bcb3b1c478da7d0cd79
cthompson4 <connort9345@gmail.com>
2022-04-11 01:50:12 -0500

    Still testing

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 app = express()
 // MongoDB database - tutorial from https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb-how-to-get-connected-to-your-database
-const {MongoClient, Db} = require('mongodb');
 randomword = "Default";
+/*const {MongoClient, Db} = require('mongodb');
+
 async function getRandomWord(client){
   max = 4; //number of documents plus 1
   random = Math.floor(Math.random() * max);
@@ -13,7 +14,7 @@ async function getRandomWord(client){
 async function main() {
 	const uri = "mongodb+srv://gamescoreapplication:gamescore@gamescore.hhvnf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
   const client = new MongoClient(uri);
-/*   try {
+   try {
     await client.connect();
     // Call function
     await  getRandomWord(client);
@@ -23,11 +24,10 @@ async function main() {
   } finally {
     await client.close();
   }
-*/
 }
 main().catch(console.error); 
 //
-
+*/
 var url = require('url');
 const path = require('path');
 
