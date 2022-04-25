39ea53006b4811f39c395ef5c82e4eafe2948bf7
efrengtoledo <efrengtoledo@lewisu.edu>
2022-04-25 11:44:55 -0500

    stable version

--- index.js
+++ index.js
@@ -51,13 +51,13 @@ const { stringify } = require('querystring');
 
 const port = process.env.PORT || 3000
 // const bcrypt = require('bcrypt') // BREAKS SITE
-const passport = require('passport')    // This does not cause issue
-const flash = require('express-flash') // Does not cause issue
-const session = require('express-session') // No
+//const passport = require('passport')    // This does not cause issue
+//const flash = require('express-flash') // Does not cause issue
+//const session = require('express-session') // No
 // const methodOverride = require('method-override') // BREAKS SITE
 //var userScore = 0 // no
 
-const initializePassport = require('./passport-config'); // Not this one
+//const initializePassport = require('./passport-config'); // Not this one
 /*
 const { initialize } = require('passport');
 initializePassport(
